bc0a92ac61ea730e83ef58611fd9999eda6c5299
Nelson Rowley <nelson@Nelsons-MacBook-Pro.local>
2020-10-16 21:38:25 +0100

    add effectHook

--- src/components/Reddit.jsx
+++ src/components/Reddit.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 export default function Reddit({ subReddit }) {
+  useEffect(() => {
+    console.log("it changed");
+  });
   return (
     <div>
       <h1>{subReddit}</h1>
