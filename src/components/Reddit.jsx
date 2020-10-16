e068b2996d6bb50ed6c8309784d03832f42842cd
Nelson Rowley <nelson@Nelsons-MacBook-Pro.local>
2020-10-16 23:40:09 +0100

    implement useEffect hook

--- src/components/Reddit.jsx
+++ src/components/Reddit.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 export default function Reddit({ subReddit }) {
+  const getData = () => {
+    fetch(`https://www.reddit.com/r/${subReddit}.json`, {
+      method: "GET",
+      mode: "cors",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => {
+        //TODO: pass error to navbar component?
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
-    console.log("it changed");
+    //prevent fetch called when page loads
+    if (subReddit != null) {
+      getData();
+    }
   });
   return (
     <div>
